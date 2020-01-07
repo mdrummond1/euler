4693e341bf0283c670fe1a0031dc54ab82a31bce
Matthew Drummond <mattatatter@gmail.com>
2020-01-07 16:15:05 -0600

    work on solving with list comprehension

--- lrgPrimeFactor.py
+++ lrgPrimeFactor.py
@@ -6,3 +6,14 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 '''
 
+def factor(term):
+    factors = [i for i in range(1, term) if term % i == 0]
+    print(factors)
+    return factors
+
+num = 12
+factors = factor(num)
+factors.reverse()
+print(factors)
+top = factors[0]
+print("max is %i" % (top))
